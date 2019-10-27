9923e6aa581d61671af22eee991cdb9f4b78bf22
Duane Robertson <duane@duanerobertson.com>
2019-10-27 11:02:45 -0500

    Get OnDamage running in custom script.
    This may not be correct. I didn't have to specify UnitScript previously.

--- src/server/scripts/Custom/custom_make_instances_easier.cpp
+++ src/server/scripts/Custom/custom_make_instances_easier.cpp
@@ -1,84 +1,92 @@
+#include "Chat.h"
 #include "ScriptMgr.h"
 #include "Pet.h"
+
+
+#define VERBOSE 0
 
 
 // This assumes that every dungeon is five-man, and may not be
 //  terribly efficient. (Not an issue for me.)
 
-class custom_make_instances_easier : public PlayerScript
+class custom_make_instances_easier : public PlayerScript, UnitScript
 {
-  public:
-    custom_make_instances_easier() : PlayerScript("custom_make_instances_easier") { }
+    public:
+        custom_make_instances_easier() : PlayerScript("custom_make_instances_easier"), UnitScript("custom_make_instances_easier") { }
 
-    void OnDamage(Unit* attacker, Unit* victim, uint32& damage)
-    {
-        /*
-        if (attacker && victim)
+        void OnDamage(Unit* attacker, Unit* victim, uint32& damage)
         {
-            ObjectGuid ag = attacker->GetGUID();
-            if (ag.IsPlayer() && attacker->getClass() == CLASS_WARLOCK)
+            /*
+            if (attacker && victim)
             {
-                damage *= 3;
+                ObjectGuid ag = attacker->GetGUID();
+                if (ag.IsPlayer() && attacker->getClass() == CLASS_WARLOCK)
+                {
+                    damage *= 3;
+                }
+            }
+            */
+
+            if (!victim || !victim->GetMap()->IsDungeon())
+                return;
+
+            uint32 count = victim->GetMap()->GetPlayersCountExceptGMs();
+            if (count > 3)
+                return;
+
+            float mult = 2.0;
+            if (count == 2)
+                mult = 1.5;
+            if (count == 3)
+                mult = 1.25;
+
+            ObjectGuid vg = victim->GetGUID();
+            if (vg.IsPlayer() || (vg.IsPet() && (victim->GetOwnerGUID()).IsPlayer()))
+            {
+                damage /= mult;
+
+                if (VERBOSE && vg.IsPlayer())
+                    ChatHandler(((Player *) victim)->GetSession()).PSendSysMessage("You take %d damage. It would have been %d.\n", damage, (int)(damage * mult));
+
+                return;
+            }
+
+            if (!attacker)
+                return;
+
+            ObjectGuid ag = attacker->GetGUID();
+            if (ag.IsPlayer() || (ag.IsPet() && (attacker->GetOwnerGUID()).IsPlayer()))
+            {
+                damage *= mult;
+
+                if (VERBOSE && ag.IsPlayer())
+                    ChatHandler(((Player *) attacker)->GetSession()).PSendSysMessage("You do %d damage. It would have been %d.\n", damage, (int)(damage / mult));
+
+                return;
             }
         }
-        */
 
-        if (!victim || !victim->GetMap()->IsDungeon())
-            return;
 
-        uint32 count = victim->GetMap()->GetPlayersCountExceptGMs();
-        if (count > 3)
-            return;
-
-        float mult = 2.0;
-        if (count == 2)
-            mult = 1.5;
-        if (count == 3)
-            mult = 1.25;
-
-        ObjectGuid vg = victim->GetGUID();
-        if (vg.IsPlayer() || (vg.IsPet() && (victim->GetOwnerGUID()).IsPlayer()))
+        void OnGiveXP(Player* player, uint32& amount, Unit* victim)
         {
-            //printf("Normal damage %d\n", damage);
-            damage /= mult;
-            //printf("Player takes %d damage\n", damage);
-            return;
+            if (!player || !player->GetMap()->IsDungeon())
+                return;
+
+            uint32 count = player->GetMap()->GetPlayersCountExceptGMs();
+            if (count > 3)
+                return;
+
+            uint32 mult = 5;
+
+            if (count == 1)
+                mult = 20;
+
+            //printf("experience lowered from %d to %d\n", amount, amount / 5);
+            amount /= mult;
         }
-
-        if (!attacker)
-            return;
-
-        ObjectGuid ag = attacker->GetGUID();
-        if (ag.IsPlayer() || (ag.IsPet() && (attacker->GetOwnerGUID()).IsPlayer()))
-        {
-            //printf("Normal damage %d\n", damage);
-            damage *= mult;
-            //printf("Player deals %d damage\n", damage);
-            return;
-        }
-    }
-
-
-    void OnGiveXP(Player* player, uint32& amount, Unit* victim)
-    {
-        if (!player || !player->GetMap()->IsDungeon())
-            return;
-
-        uint32 count = player->GetMap()->GetPlayersCountExceptGMs();
-        if (count > 3)
-            return;
-
-        uint32 mult = 2;
-
-        if (count == 1)
-            mult = 3;
-
-        //printf("experience lowered from %d to %d\n", amount, amount / 5);
-        amount /= mult;
-    }
 };
 
 void AddSC_custom_make_instances_easier()
 {
-  new custom_make_instances_easier();
+    new custom_make_instances_easier();
 }
